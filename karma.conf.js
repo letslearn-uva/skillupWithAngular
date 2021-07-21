b17f3aac1549dceeecea4ee98308f5b32422c3f0
unknown <yuvauv5613@gmail.com>
2021-07-22 00:20:28 +0530

    dr changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/skillupWithAngular'),
+      dir: require('path').join(__dirname, './coverage/skillupwithAngular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
